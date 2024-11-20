384026f58e48713dd1d82d166b520f726c8a8431
유종화 <sola201@naver.com>
2024-11-20 17:13:30 +0900

    모의고사 2번째 풀이

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -53,3 +53,48 @@ class Solution {
         return answer;
     }
 }
+
+// 두번째 풀이
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+
+        int[][] pattern = {
+
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+
+        };
+
+        int[] scores = new int[3];
+
+        for(int i = 0; i < answers.length; i++) {
+
+            for(int j = 0; j < pattern.length; j++) {
+
+                if(answers[i] == pattern[j][i % pattern[j].length]) {
+
+                    scores[j]++;
+
+                }
+
+            }
+
+        }
+
+        int result = Math.max(scores[0], Math.max(scores[1],scores[2]));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < scores.length; i++) {
+
+            if(scores[i] == result) {
+                list.add(i + 1);
+            }
+
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
