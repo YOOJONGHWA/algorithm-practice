ea4bffce685bb560be268486fcf345b3cc4ee876
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2024-12-04 16:22:07 +0900

    [level 1] Title: 카드 뭉치, Time: 1.23 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        ArrayDeque<String> cards1Deque = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> cards2Deque = new ArrayDeque<>(Arrays.asList(cards2));
+        ArrayDeque<String> goalDeque = new ArrayDeque<>(Arrays.asList(goal));
+        
+        while(!goalDeque.isEmpty()) {
+            
+            if(!cards1Deque.isEmpty() && cards1Deque.peekFirst().equals(goalDeque.peekFirst())) {
+                cards1Deque.pollFirst();
+                goalDeque.pollFirst();
+            }
+            else if(!cards2Deque.isEmpty() && cards2Deque.peekFirst().equals(goalDeque.peekFirst())) {
+                cards2Deque.pollFirst();
+                goalDeque.pollFirst();
+            }
+            else {
+                break;
+            }
+            
+        }
+
+        return goalDeque.isEmpty() ? "Yes" : "No";
+    }
+}
