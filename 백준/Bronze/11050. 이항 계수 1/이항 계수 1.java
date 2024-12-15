21237a22583a83c0e93fa015c4d6cd74354f865c
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2024-12-15 23:47:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,27 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(nkFactorial(n) / (nkFactorial(k) * nkFactorial(n - k)));
+
+    }
+
+    private static int nkFactorial(int i) {
+        if (i <= 0) return 1;
+        return i * nkFactorial(i - 1);
+    }
+
+}
