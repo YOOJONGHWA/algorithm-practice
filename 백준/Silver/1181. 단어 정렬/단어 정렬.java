96009b2da00a8cd7180ffed1c209434344cac8bc
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2024-12-16 15:52:18 +0900

    [Silver V] Title: 단어 정렬, Time: 524 ms, Memory: 25032 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,39 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        list.sort((a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            }
+            return Integer.compare(a.length(), b.length());
+        });
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        br.close();
+    }
+
+}
