ede12023f323905f1084b02b63ed44644f5df8b6
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2025-01-06 17:22:20 +0900

    [Bronze III] Title: 세 수, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,43 +1,23 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[] arr = new int[3];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        int[] arr = new int[3];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(findNumber(arr));
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+
     }
-
-    private static int findNumber(int[] arr) {
-
-        for (int i = 0; i < arr.length - 1; i++) {
-
-            for (int j = 0; j < arr.length -1 - i; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-
-            }
-
-        }
-
-        return arr[1];
-    }
-
 }
