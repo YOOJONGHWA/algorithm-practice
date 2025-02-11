cfe95064a9157300a0deadd9fa4c4a430dbe75df
유종화 <121346218+YOOJONGHWA@users.noreply.github.com>
2025-02-11 22:05:53 +0900

    [Silver III] Title: 소수 구하기, Time: 232 ms, Memory: 21800 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,43 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
 
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int first = Integer.parseInt(st.nextToken());
+        int last = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[last + 1];
+        for (int i = 2; i <= last; i++) {
+            isPrime[i] = true;
+        }
+
+        int sqrtLast = (int) Math.sqrt(last);
+
+        for (int i = 2; i <= sqrtLast; i++) {
+            if (!isPrime[i]) continue;
+
+            for (int j = i * i; j <= last; j += i) {
+                isPrime[j] = false;
+            }
+        }
 
         StringBuilder sb = new StringBuilder();
-
-        for (int i = n; i <= m; i++) {
-            if (isPrime(i)) {
+        for (int i = first; i <= last; i++) {
+            if (isPrime[i]) {
                 sb.append(i).append("\n");
             }
         }
 
         System.out.print(sb);
-    }
-    
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false; 
-        }
-        return true; 
     }
 }
